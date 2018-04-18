c85d569a3a1bee74e6de6a2add05821e95fab57e
Andi Hofmann <andreas-h-online@web.de>
2018-04-18 10:52:50 +0200

    Aufgabe 2 Stack-Iterator

--- src/main/java/ueb04/StackImpl.java
+++ src/main/java/ueb04/StackImpl.java
@@ -4,47 +4,67 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 class StackImpl<T> implements Stack<T> {
-	/**
-	 * Gibt einen Reverse-Iterator zurück, d.h. ein Iterator, welcher die Elemente
-	 * in umgekehrter Reihenfolge zurück gibt. Also als erstes das letzte (also "unterste"),
-	 * Element, dann das zweit-unterste etc.
-	 */
-	@Override
-	public Iterator<T> iterator() {
-		// Iterator implementieren...
-		throw new UnsupportedOperationException();
-	}
+    /**
+     * Gibt einen Reverse-Iterator zurück, d.h. ein Iterator, welcher die Elemente
+     * in umgekehrter Reihenfolge zurück gibt. Also als erstes das letzte (also "unterste"),
+     * Element, dann das zweit-unterste etc.
+     */
+    @Override
+    public Iterator<T> iterator() {
+        return new Iterator<T>() {
+            Stack<Element> revAgenda = new StackImpl<>();
 
-	private class Element {
-		T value;
-		Element next;
-		Element(T value, Element next) {
-			this.value = value;
-			this.next = next;
-		}
-	}
+            {
+                Element it = top;
+                while(it != null){
+                    revAgenda.push(it);
+                    it = it.next;
+                }
+            }
 
-	private Element top;
-	private int s = 0;
+            @Override
+            public boolean hasNext() {
+                return revAgenda.size() > 0;
+            }
 
-	@Override
-	public void push(T c) {
-		top = new Element(c, top);
-		s++;
-	}
+            @Override
+            public T next() {
+                return revAgenda.pop().value;
+            }
+        };
+    }
 
-	@Override
-	public T pop() {
-		if (top == null)
-			throw new NoSuchElementException();
-		T v = top.value;
-		top = top.next;
-		s--;
-		return v;
-	}
+    private class Element {
+        T value;
+        Element next;
 
-	@Override
-	public int size() {
-		return s;
-	}
+        Element(T value, Element next) {
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private Element top;
+    private int s = 0;
+
+    @Override
+    public void push(T c) {
+        top = new Element(c, top);
+        s++;
+    }
+
+    @Override
+    public T pop() {
+        if (top == null)
+            throw new NoSuchElementException();
+        T v = top.value;
+        top = top.next;
+        s--;
+        return v;
+    }
+
+    @Override
+    public int size() {
+        return s;
+    }
 }
